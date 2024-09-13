f6fcd07988d3b1000338ba46e03eeba3cb3d8671
Marcos Vinícius <mvinic2011@gmail.com>
2024-09-12 22:38:11 -0300

    database connection

--- src/database/tables.ts
+++ src/database/tables.ts
@@ -1,4 +1,26 @@
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
 
-import { sqlite } from "./config";
+export const createTables = async () => {
+    const db = await open({
+        filename: './src/database/database.db',
+        driver: sqlite3.Database
+    });
 
-// CREATE TABLES HERE
+    await db.exec(`CREATE TABLE IF NOT EXISTS Cliente (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome VARCHAR(100) NOT NULL,
+        pais VARCHAR(75) NOT NULL,
+        estado VARCHAR(50) NOT NULL,
+        cidade VARCHAR(50) NOT NULL,
+        rua VARCHAR(100) NOT NULL,
+        numero INTEGER NOT NULL,
+        complemento VARCHAR(100),
+        cep VARCHAR(9) NOT NULL,
+        limiteCredito REAL NOT NULL,
+        dataCadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+    )`);
+
+    console.log('Tables created or already exist.');
+    await db.close();
+}
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './http/routes';
+import { createTables } from './database/tables';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(cors());
-
+app.use(bodyParser.json());
 app.use(routes);
 
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+createTables().then(() => {
+    const port = 3000;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}).catch(error => {
+    console.error('Error creating tables:', error);
 });
